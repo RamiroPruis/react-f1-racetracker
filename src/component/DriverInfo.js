1b063b1ad36649a8755ceaa38a9a4ea9f1da5b26
Ramiro Pruis <ramiro_pruis@hotmail.com>
2022-01-05 21:58:11 -0300

    fetch optimization

--- src/component/DriverInfo.js
+++ src/component/DriverInfo.js
@@ -5,50 +5,46 @@ import noProfile from '../img/noPic.png'
 import {SCUDERIA_COLOR} from '../utils/utils'
 import { useEmiiter } from "../context/Emitter"
 
-function DriverInfo({id}) {
+function DriverInfo({driver}) {
     const [searchParams,setSearchParams] = useSearchParams()
-    const [driverName,setDriverName] = useState('')
     const [profileImage,setProfileImage] = useState('')
     const [scuderiaId,setScuderiaId] = useState('')
     const {setDataEvent} = useEmiiter()
 
-    const fetchData = ()=>{
-        axios.get(`http://ergast.com/api/f1/drivers/${id}.json`)
-        .then((response)=>{
-            const driverData = response.data.MRData.DriverTable.Drivers.pop()
-            setDriverName(driverData.code)
-            const wikiId = driverData.url.split('/').pop()
-            axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&titles=${wikiId}&prop=pageimages&format=json&pithumbsize=100`)
+
+        
+
+ 
+    useEffect(()=>{
+        const wikiId = driver.url.split('/').pop()
+        
+        axios.get(`https://en.wikipedia.org/w/api.php?&origin=*&action=query&titles=${wikiId}&prop=pageimages&format=json&pithumbsize=100`)
             .then((wikiData)=>{
                 const pageID = Object.keys(wikiData.data.query.pages)
                 const imgSource = wikiData.data.query.pages[pageID].thumbnail.source
                 setProfileImage(imgSource)
             })
-            .catch((e)=>{
-                setProfileImage(noProfile)
-            })
-        })
+        
+        
+        
+    },[driver.url])
 
-        axios.get(`http://ergast.com/api/f1/${searchParams.get('year')}/drivers/${id}/constructors.json`)
+    useEffect(()=>{
+        axios.get(`http://ergast.com/api/f1/${searchParams.get('year')}/${searchParams.get('race')}/drivers/${driver.driverId}/constructors.json`)
             .then(response=>{
                 const value = response.data.MRData.ConstructorTable.Constructors[0].constructorId
                 setScuderiaId(value)
                 setDataEvent(SCUDERIA_COLOR[value])
             })
-    }
-
-
-    useEffect(()=>{
-        fetchData()
-    },)
+    },[])
 
 
     return (
-        <div className="flex items-center space-x-2 w-28">
-            <div className="rounded-full h-10 w-10 ">
+        <div className="flex transition-all hover:scale-125 hover:cursor-default items-center space-x-2 w-28">
+            <div className=" rounded-full h-10 w-10 ">
              <img className={` rounded-full h-10 w-10 object-cover border-gray-500' border-2`} style={{borderColor: `${SCUDERIA_COLOR[scuderiaId]}`}} src={profileImage} alt=""/>
             </div>
-            <h1 className=" text-left font-f1 font-semibold">{driverName}</h1>
+            <h1 className=" text-left font-f1 font-semibold">{driver.code}</h1>
         </div>
     )
 }
